a7446f14f9dcf7be11d0d119927792d30ea3df43
kthom-pi <64857144+kthom-pi@users.noreply.github.com>
2024-03-16 16:48:08 -0500

    Removed the demographics data

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ import numpy as np
 import streamlit as st
 import altair as alt
 import datetime
+import plotly.express as px
 
 
 # Import the data files.
 # Crime database
 df_crime = pd.read_csv("C:\\Users\\kthom\\Desktop\\Personal Projects\\Chicago Crime Streamlit\\data_files\\Crimes_-_2001_to_Present.csv")
-# Demographics database
-df_demographics = pd.read_csv("C:\\Users\\kthom\\Desktop\\Personal Projects\\Chicago Crime Streamlit\\data_files\\Community_Data_Snapshots_2023_-7949553649742586148.csv")
+
 # Communities database
 df_communities = pd.read_csv("C:\\Users\\kthom\\Desktop\\Personal Projects\\Chicago Crime Streamlit\\data_files\\communities.csv")
 
@@ -18,13 +18,14 @@ df_communities = pd.read_csv("C:\\Users\\kthom\\Desktop\\Personal Projects\\Chic
 df_crime_1 = df_crime.loc[:1000, :]
 
 crime = "MOTOR VEHICLE THEFT"
+community = "Rogers Park"
 
 start_date = datetime.date(2018, 1, 1)
 end_date = datetime.date(2024, 1, 10)
 
 
 @st.cache_data
-def clean_robberies(crime_df, demographics, neighborhoods, crime_type):
+def clean_robberies(crime_df, neighborhoods, crime_type):
     """Combines the crime, demographic and neightborhoods dataframe into one."""
 
     # Convert 'Date' column to date time
@@ -64,13 +65,30 @@ def clean_robberies(crime_df, demographics, neighborhoods, crime_type):
     # Add the communities to the dataframe.
     crime_df_1 = crime_df.merge(neighborhoods, how='left', on='Community Area')
 
-    # Change the column name for the demographics from GEOG to 'Community'.
-    demographics = demographics.rename(columns={'GEOG':'Community'})
+    return crime_df_1
 
-    # Add the demographics dataframe
-    crime_df_2 = crime_df_1.merge(demographics, how='left', on='Community')
 
-    return crime_df_2
+@st.cache_data
+def plot_community_time_day(df, community, begin_year, end_year):
+    """Determines and plots the number of crimes in the community by the time of day."""
+
+    # filter for the number of years
+    mask_years = (df['Year'] >= begin_year) & (df['Year'] < end_year)
+
+    # filter for the community
+    mask_community = (df['Community'] == community)
+
+    # Apply the years and community masks to the the dataframe
+    df = df.loc[mask_years & mask_community]
+
+    # Order for the x_axis
+    order = ['12am to 4am', '4am to 8am', '8am to 12pm', '12pm to 4pm', '4pm to 8pm', '8pm to 12am']
+
+    # Plot the crimes by time of day
+    fig = px.histogram(df, x='Time of Day', category_orders={'Time of Day': order})
+
+    return fig
+
 
 
 st.set_page_config(
@@ -88,18 +106,41 @@ with st.sidebar:
     begin_date = st.date_input('Begin Date', start_date)
     end_date = st.date_input('End Date', end_date)
 
+# Extract the year
+begin_year_int = begin_date.year
+end_year_int = end_date.year
+
 data_load_state = st.text('Loading data...')
-new_df = clean_robberies(df_crime_1, df_demographics, df_communities, crime)
+new_df = clean_robberies(df_crime_1, df_communities, crime)
 
 
 st.write(new_df)
 
-
-
-
-
 st.subheader('Crime by Time of Day')
 
+time_of_day_plot = plot_community_time_day(new_df, community, begin_year_int, end_year_int)
+
+# Initialize variables to hold selected begin and end years
+begin_year_value = None
+end_year_value = None
+
+
+# Button to trigger the update
+update_button = st.button("Update Years")
+
+if update_button:
+    community = st.selectbox('Select Community', options=new_df['Community'].unique())
+    begin_year_value = st.number_input('Begin Year', min_value=int(new_df['year'].min()), value=int(new_df['year'].min()), step=1)
+    end_year_value = st.number_input('End Year', min_value=begin_year_value, value=int(new_df['year'].max()), step=1)
+
+# Display selected or default values
+st.write(f"Begin Year: {begin_year_value if begin_year_value else int(new_df['year'].min())}")
+st.write(f"End Year: {end_year_value if end_year_value else int(new_df['year'].max())}")
+
+if st.button('Plot'):
+
+    fig = plot_community_time_day(new_df, community, begin_year_int, end_year_int)
+    st.plotly_chart(fig)
 
 
 
