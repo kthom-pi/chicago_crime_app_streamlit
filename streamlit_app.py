bc46d876cd0296497a8767233b745be7241392d2
kthom-pi <64857144+kthom-pi@users.noreply.github.com>
2024-03-17 20:19:38 -0500

    Have both the data preview and barplots working.

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,20 +7,33 @@ import datetime
 import plotly.express as px
 
 
-# Import the data files.
-# Crime database
-df_crime = pd.read_csv("C:\\Users\\kthom\\Desktop\\Personal Projects\\Chicago Crime Streamlit\\data_files\\Crimes_-_2001_to_Present.csv")
-# Communities database
+# Import the community data files.
 df_communities = pd.read_csv("C:\\Users\\kthom\\Desktop\\Personal Projects\\Chicago Crime Streamlit\\data_files\\communities.csv")
 
-df_crime_1 = df_crime
+@st.cache_data
+def mod_df():
+    """Imports the dataframe and keeps only the desired columns."""
+
+    # Crime database
+    df_crime = pd.read_csv(
+        "C:\\Users\\kthom\\Desktop\\Personal Projects\\Chicago Crime Streamlit\\data_files\\crime_df_mod.csv")
+
+    # Filter for the crimes of interest.
+    keep_crimes = ['THEFT', 'ASSAULT', 'SEX OFFENSE', 'BURGLARY', 'CRIM SEXUAL ASSAULT',
+                   'MOTOR VEHICLE THEFT', 'OFFENSE INVOLVING CHILDREN', 'CRIMINAL TRESPASS',
+                   'ROBBERY', 'CRIMINAL SEXUAL ASSAULT', 'STALKING', 'HOMICIDE', 'KIDNAPPING',
+                   'DOMESTIC VIOLENCE']
+
+    df_crime_input = df_crime[df_crime['Primary Type'].isin(keep_crimes)]
+
+    return df_crime_input
 
 @st.cache_data
 def clean_robberies(crime_df, neighborhoods, community, crime, start_date= "2018-01-01", end_date="2024-01-01"):
     """Combines the crime, demographic and neightborhoods dataframe into one."""
 
     # Convert 'Date' column to date time
-    crime_df['Date'] = pd.to_datetime(crime_df['Date'])
+    crime_df['Date'] = pd.to_datetime(crime_df['Date'], format='mixed')
 
     # Apply filter to dataframe
     mask_1 = crime_df['Date'] >= start_date
@@ -58,15 +71,7 @@ def clean_robberies(crime_df, neighborhoods, community, crime, start_date= "2018
     # Filter for the community
     crime_df_2 = crime_df_1.loc[crime_df_1["Community"] == community]
 
-    # Filter for the crimes of interest.
-    keep_crimes = ['THEFT', 'ASSAULT', 'SEX OFFENSE', 'BURGLARY', 'CRIM SEXUAL ASSAULT',
-                   'MOTOR VEHICLE THEFT', 'OFFENSE INVOLVING CHILDREN', 'CRIMINAL TRESPASS',
-                   'ROBBERY', 'CRIMINAL SEXUAL ASSAULT', 'STALKING', 'HOMICIDE', 'KIDNAPPING',
-                   'DOMESTIC VIOLENCE']
-
-    crime_df_3 = crime_df_2[crime_df_2['Primary Type'].isin(keep_crimes)]
-
-    return crime_df_3
+    return crime_df_2
 
 
 def plot_community_time_day(df):
@@ -94,38 +99,56 @@ end_init = "2024-01-01"
 start_init_1 = pd.to_datetime(start_init)
 end_init_1 = pd.to_datetime(end_init)
 
+
+# Obtain the dataset with the desired columns
+df_crime_1 = mod_df()
+
+
 # Add a sidebar
 with st.sidebar:
     st.title('Neighborhodd Input')
     begin_date = st.date_input('Begin Date', start_init_1)
     ending_date = st.date_input('End Date', end_init_1)
     community_chosen = st.selectbox('Community', options=df_communities['Community'].unique())
-    crime_type = st.selectbox('Crime Type', options=df_crime['Primary Type'].unique())
+    crime_type = st.selectbox('Crime Type', options=df_crime_1['Primary Type'].unique())
+    data_button = st.button("Preview Data")
+    barplot_button = st.button('Plot Crimes by Time of Day')
 
 # Convert the datetime back to a string
 begin_date_1 = begin_date.strftime('%Y-%m-%d')
 ending_date_1 = ending_date.strftime('%Y-%m-%d')
 
-data_load_state = st.text('Loading data...')
-new_df = clean_robberies(df_crime_1, df_communities, community_chosen, crime_type, begin_date_1, ending_date_1)
-data_load_state.text("Data loaded!")
 
-st.write(new_df)
-st.subheader('Crime by Time of Day')
+# Create a blank dataframe to hold in the session state
+# Obtain the column names from the original crime dataframe
+column_names = mod_df().columns
+blank_df = pd.DataFrame(columns=column_names)
+st.session_state['new_df'] = blank_df
 
-# Extract the year
-begin_year_int = begin_date.year
-end_year_int = ending_date.year
+
+if data_button:
+    data_load_state = st.text('Loading data...')
+    new_df_1 = clean_robberies(df_crime_1, df_communities, community_chosen, crime_type, begin_date_1, ending_date_1)
+    data_load_state.text("Data loaded!")
+    st.session_state['new_df_1'] = new_df_1
+
+st.write(st.session_state['new_df_1'])
+
+if barplot_button:
+    st.subheader('Crime by Time of Day')
+    fig = plot_community_time_day(st.session_state['new_df_1'])
+    st.plotly_chart(fig)
+
+
+
+
 
 
 # Display selected or default values
-st.write(f"Begin Year: {begin_year_int if begin_year_int else int(new_df['year'].min())}")
-st.write(f"End Year: {end_year_int if end_year_int else int(new_df['year'].max())}")
+#st.write(f"Begin Year: {begin_year_int if begin_year_int else int(new_df['year'].min())}")
+#st.write(f"End Year: {end_year_int if end_year_int else int(new_df['year'].max())}")
 
-if st.button('Plot'):
 
-    fig = plot_community_time_day(new_df)
-    st.plotly_chart(fig)
 
 
 
